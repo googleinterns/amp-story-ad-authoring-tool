ba13383c05c284cc72221832604c626e39e2689e
amakhdoom <amakhdoom@google.com>
2020-08-07 14:18:46 -0400

    pr comment changes

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,7 +1,4 @@
 import {Component} from '@angular/core';
-import {AdAuthoringService} from '../ad-authoring/ad-authoring.service';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-download',
@@ -9,21 +6,12 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./download.component.scss'],
 })
 export class DownloadComponent {
-  fileUrl;
-  ampHtmlObs: Observable<string>;
-
-  constructor(private service: AdAuthoringService) {
-    this.ampHtmlObs = service
-      .getAdAuthorings()
-      .pipe(map(state => state.AMPHTMLstring));
-  }
-
   downloadFile() {
     // TODO: create a service that generates the AMP ad html instead of this dummy data
     const data = 'amp story ad html goes here';
-    const a = document.createElement('a');
     const blob = new Blob([data], {type: 'application/octet-stream'});
     const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
     document.body.appendChild(a);
     a.style.display = 'none';
     a.href = url;

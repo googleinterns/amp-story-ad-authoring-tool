fe1814ff39710294c22145a890decd7f692adbdd
amakhdoom <amakhdoom@google.com>
2020-08-11 16:29:46 -0400

    deleted unnecessary code

--- src/app/asset-upload/asset-upload.component.ts
+++ src/app/asset-upload/asset-upload.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {AssetUploadService} from './asset-upload.service';
-import {Observable} from 'rxjs';
-import {AdAuthoringWorkflowState} from '../ad-authoring/ad-authoring.state';
 
 @Component({
   selector: 'app-asset-upload',
@@ -9,14 +7,9 @@ import {AdAuthoringWorkflowState} from '../ad-authoring/ad-authoring.state';
   styleUrls: ['./asset-upload.component.scss'],
 })
 export class AssetUploadComponent {
-  adAuthoringObs: Observable<AdAuthoringWorkflowState>;
-  // raw base 64 encoding of an image or video
-  rawAssetBase64 = '';
   file: File | null = null;
 
-  constructor(private service: AssetUploadService) {
-    this.adAuthoringObs = service.getAssets();
-  }
+  constructor(private service: AssetUploadService) {}
 
   onFileInput(fileInput: any) {
     this.file = fileInput.target.files[0];

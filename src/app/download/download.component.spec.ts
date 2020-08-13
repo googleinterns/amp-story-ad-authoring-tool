691a62a97bcef05266e50a3e8143fe57f12c78b6
amakhdoom <amakhdoom@google.com>
2020-08-13 12:59:42 -0400

    added more tests

--- src/app/download/download.component.spec.ts
+++ src/app/download/download.component.spec.ts
@@ -5,6 +5,8 @@ import {MatButtonHarness} from '@angular/material/button/testing';
 import {HarnessLoader} from '@angular/cdk/testing';
 import {AppModule} from '../app.module';
 import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatExpansionPanelHarness} from '@angular/material/expansion/testing';
+import {MatInputHarness} from '@angular/material/input/testing';
 
 let loader: HarnessLoader;
 
@@ -35,4 +37,25 @@ describe('DownloadComponent', () => {
 
     expect(component.downloadFileZip).toHaveBeenCalled();
   });
+
+  // it('should contain a defined landingUrl element', async() => {
+  //   // expand the call to action expansion panel
+  //   const callToActionExpansion = await loader.getHarness(
+  //     MatExpansionPanelHarness.with({
+  //       selector: '#call-to-action-panel',
+  //     })
+  //   );
+  //   await callToActionExpansion.expand();
+  //   // set landingUrl value
+  //   const landingUrlInput = await loader.getHarness(MatInputHarness);
+  //   await landingUrlInput.setValue('https://google.com');
+  //   // expand the asset upload expansion panel
+  //   const assetUploadExpansion = await loader.getHarness(
+  //     MatExpansionPanelHarness.with({
+  //       selector: '#asset-upload-panel',
+  //     })
+  //   );
+  //   await assetUploadExpansion.expand();
+  //   // upload an image
+  // });
 });
--- src/app/download/download.service.spec.ts
+++ src/app/download/download.service.spec.ts
@@ -1,16 +1,55 @@
 import {TestBed} from '@angular/core/testing';
 
 import {DownloadService} from './download.service';
+import {AdAuthoringWorkflowStateContainer} from '../ad-authoring/ad-authoring.state';
 
 describe('DownloadService', () => {
   let service: DownloadService;
+  let state: AdAuthoringWorkflowStateContainer;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DownloadService);
+    state = TestBed.inject(AdAuthoringWorkflowStateContainer);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('generateHtmlForDownload should contain landingUrl value saved in state', () => {
+    const adHtml = service.generateHtmlForDownload();
+
+    expect(adHtml).toContain(state.getValue().landingUrl);
+  });
+
+  it('generateHtmlForDownload should contain landingType value saved in state', () => {
+    const adHtml = service.generateHtmlForDownload();
+
+    expect(adHtml).toContain(state.getValue().landingType);
+  });
+
+  it('generateHtmlForDownload should contain landingUrl value saved in state', () => {
+    const adHtml = service.generateHtmlForDownload();
+
+    expect(adHtml).toContain(state.getValue().landingUrl);
+  });
+
+  it('generateHtmlForDownload should contain callToAction value saved in state', () => {
+    const adHtml = service.generateHtmlForDownload();
+
+    expect(adHtml).toContain(state.getValue().callToAction);
+  });
+
+  it('generateHtmlForDownload should contain landingUrl value saved in state', () => {
+    const adHtml = service.generateHtmlForDownload();
+
+    expect(adHtml).toContain(state.getValue().file.name);
+  });
+
+  it('should get null asset file initially', () => {
+    const assetFile = service.getAsset();
+
+    expect(assetFile).toBe(null);
   });
 });

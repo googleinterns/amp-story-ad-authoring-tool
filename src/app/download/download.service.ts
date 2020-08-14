582bb9a0f7c881b7fc445df903ad38e5cede3426
amakhdoom <amakhdoom@google.com>
2020-08-14 16:29:56 -0400

    moved generateAmpHtmlforDownload to generateAmpHtml file

--- src/app/download/download.service.ts
+++ src/app/download/download.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AdAuthoringWorkflowStateContainer} from '../ad-authoring/ad-authoring.state';
+import {generateHtmlForDownload} from '../ad-authoring/generate-amp-html';
 
 @Injectable({
   providedIn: 'root',
@@ -28,49 +29,13 @@ export class DownloadService {
     const file = this.adAuthoringState.getValue().file;
     const assetPath = file.name;
 
-    // TODO:
-    // If anything is undefined e.g the asset, make sure to send an error message
-    // Instead of generating amp html here, call the generateAmpHtml() from ../ad-authoring/generate-amp-html
-
-    const adHtml = `
-      <!doctype html>
-      <html amp4ads>
-        <head>
-          <meta charset=\"utf-8\">
-          <meta name=\"viewport\" content=\"width=device-width,minimum-scale=1\">
-          <meta name=\"amp-cta-type\" content=\"${callToAction}\">
-          <meta name=\"amp-cta-url\" content=\"${landingUrl}\">
-          <meta name=\"amp-cta-landing-page-type\" content=\"${landingType}\">
-          <style amp4ads-boilerplate>
-            body{visibility:hidden}
-          <\/style>
-          <script async src=\"https:\/\/cdn.ampproject.org\/amp4ads-v0.js\"><\/script>`;
-
-    const ampVideoRuntime = `
-          <script async custom-element=\"amp-video\" src=\"https:\/\/cdn.ampproject.org\/v0\/amp-video-0.1.js\"><\/script>`;
-
-    const imageHtml = `
-        <\/head>
-          <body>
-            <amp-img layout=\"fixed\" height=\"250\" width=\"300\" src=\"${assetPath}\">
-            <\/amp-img>
-          <\/body>
-      <\/html>`;
-
-    const videoHtml = file
-      ? `
-        <\/head>
-          <body>
-            <amp-video layout=\"fill\" height=\"1920\" width=\"1080\" autoplay loop>
-              <source src=\".\/${assetPath}\" type=\"${file.type}\" \/>
-            <\/amp-video>
-          <\/body>
-      <\/html>`
-      : ``;
-
-    const adAmpHtml = file?.type.includes('video')
-      ? adHtml + ampVideoRuntime + videoHtml
-      : adHtml + imageHtml;
+    const adAmpHtml = generateHtmlForDownload({
+      callToActionStr: callToAction,
+      landingUrl: landingUrl,
+      landingType: landingType,
+      assetFilePath: assetPath,
+      assetFile: file,
+    });
 
     return adAmpHtml;
   }
